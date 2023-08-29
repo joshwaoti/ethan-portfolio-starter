b204a2ce3644cda1b36957c4b3793c45592e36f6
joshwaoti <joshwaotieno643@gmail.com>
2023-08-29 12:42:13 +0300

    done with particles

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ const Home = () => {
         
       </div>
       {/* particles */}
-      <div>Particles</div>
+    <ParticlesContainer />
       {/* avatar img */}
       <motion.div variants={fadeIn('up', 0.5)} initial='hidden' animate='show' exit='hidden' translation={{duration:1, ease: 'easeInOut'}} className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
         <Avatar/>
