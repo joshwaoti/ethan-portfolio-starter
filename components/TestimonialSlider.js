f43e39780a44354f09b745e1f274b54addd040c2
joshwaoti <joshwaotieno643@gmail.com>
2023-08-31 14:46:29 +0300

    first part done

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,3 +1,27 @@
+
+
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// next image
+import Image from 'next/image';
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+
+// import required modules
+import {Navigation, Pagination} from 'swiper'
+
+// icons
+import {FaQuoteLeft} from 'react-icons/fa'
+
+// framer motion
+import {motion} from 'framer-motion'
+
+
 // testimonial data
 const testimonialData = [
   {
@@ -16,7 +40,7 @@ const testimonialData = [
   },
   {
     image: '/t-avt-3.png',
-    name: 'Jhon Doe',
+    name: 'John Doe',
     position: 'Customer',
     message:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
@@ -24,7 +48,53 @@ const testimonialData = [
 ];
 
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return <Swiper 
+  navigation={true}
+  pagination={{
+    clickable: true,
+  }}
+  modules={[Navigation,Pagination]}
+  className='h-[480px]'
+  
+  >
+    {
+      testimonialData.map((person, index)=> {
+        return <SwiperSlide key={index}>
+          
+          <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+            {/* avatar, name, position */}
+            <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center relative ms-auto xl:mx-0'>
+              <div className='flex flex-col justify-center text-center'>
+                {/* avatar */}
+                <div className='mb-2 mx-auto'>
+                  <Image src={person.image} width={100} height={100} alt='' />
+                </div>
+                {/* name */}
+                <div className='text-lg'>
+                  {person.name}
+                </div>
+                {/* position */}
+                <div className='text-[12px] uppercase font-extralight tracking-widest'>
+                  {person.position}
+                </div>
+              </div>
+            </div>
+            {/* quote  and message*/}
+            <div className='flex-1 flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:left-0 xl:before:h-[200px] relative xl:pl-20'>
+              {/* quote icon */}
+              <div className='mb-4'>
+                <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0'/>
+              </div>
+              {/* message */}
+              <div className='xl:text-lg text-center md:text-left'>{person.message}</div>
+            </div>
+          </div>
+        </SwiperSlide>
+      })
+    }
+  </Swiper>;
 };
 
 export default TestimonialSlider;
+
+
--- pages/about/index.js
+++ pages/about/index.js
@@ -137,7 +137,7 @@ const About = () => {
             exit='hidden'
             className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 xl:px-0">
             7 years ago, I began freelancing as a developer. Since then, I've done remote work for agencies, 
-            cosulted for startups, and collaborated
+            consulted for startups, and collaborated
             digital products for business and consumer use.</motion.p>
             {/* counters */}
             <motion.div
