68836a9478b6da3c059c7d53fbd54f6937d78ef0
joshwaoti <joshwaotieno643@gmail.com>
2023-09-01 13:32:02 +0300

    final wrap

--- components/Avatar.js
+++ components/Avatar.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 const Avatar = () => {
   return (
   <div className="hidden xl:flex xl:max-w-none">
-    <Image src={'/avatar.png'} alt="" width={737} height={678} className="translate-z-0 w-full h-full"/>
+    <Image src={'/avatar.jpg'} alt="" width={737} height={678} className="translate-z-0 w-full h-full"/>
   </div>
   );
 };
