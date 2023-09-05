8a5d526d477d40a78102dd1dc34f17674bd62fa0
Josh <joshwaotieno643@gmail.com>
2023-09-05 12:21:33 +0300

    pis

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-  <div className="hidden xl:flex xl:max-w-none">
+  <div className="hidden xl:flex mix-blend-color-dodge xl:max-w-none">
     <Image src={'/avatar.jpg'} alt="" width={737} height={678} className="translate-z-0 w-full h-full"/>
   </div>
   );
